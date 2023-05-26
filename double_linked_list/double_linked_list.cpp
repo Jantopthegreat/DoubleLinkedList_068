68466b60ff060fe57f637fea72fda87f4f929bdf
Jantopthegreat <fauzangriya@gmail.com>
2023-05-26 23:15:31 +0700

    doublelinked list retraverse

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -112,4 +112,21 @@ void doubleLinkedlist::traverse() {
 		}
 	}
 }
+void DoubleLinkedlist::revtraverse() {
+	if (listEmpty()) {
+		cout << "\nList is empty" << endl;
+	}
+
+	else {
+		cout << "\nRecord in descending order of roll number are:" << endl;
+		Node* currentNode = START;
+		while (currentNode->next != NULL)
+			currentNode = currentNode->next;
+
+		while (currentNode != NULL) {
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->prev;
+		}
+	}
+}
 
