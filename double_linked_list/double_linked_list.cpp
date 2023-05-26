409b41c4b8be6579aea5c9cc35a28cec035acbb1
Jantopthegreat <fauzangriya@gmail.com>
2023-05-26 23:30:32 +0700

    catch

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -197,3 +197,10 @@ int main() {
 			default:
 				cout << "\nInvalid option" << endl;
 				break;
+			}
+		}
+		catch (exception& e) {
+			cout << "Check for the values entered." << endl;
+		}
+	}
+}
