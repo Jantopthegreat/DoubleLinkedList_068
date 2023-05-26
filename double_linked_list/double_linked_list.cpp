870f5fa5c39482dc28743cea277aba3d3107e908
Jantopthegreat <fauzangriya@gmail.com>
2023-05-26 16:27:17 +0700

    membuat traverse double linked list

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -95,3 +95,21 @@ bool doubleLinkedlist::deleteNode(int rollNo) {
 	return true;
 }
 
+bool doubleLinkedlist::listEmpty() {
+	return (START = NULL);
+}
+
+void doubleLinkedlist::traverse() {
+	if (listEmpty())
+		cout << "\nList is empty" << endl;
+	else {
+		cout << "\nRecords in accending order of roll number are: " << endl;
+		Node* currentNode = START;
+		while (currentNode != NULL) {
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->next;
+
+		}
+	}
+}
+
